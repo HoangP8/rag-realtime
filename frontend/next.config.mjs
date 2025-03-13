7c1fb5702a171f5ea5ff9b7341833473e07ab74b
Hoang Phan <21hoang.p@vinuni.edu.vn>
2025-03-13 07:17:12 +0700

    update frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Load environment variables from .env.local
+dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
+
+const nextConfig = {
+};
 
 export default nextConfig;
